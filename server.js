83c1112a66add2a3036c10d34063860cd80199a5
Satyam <ap8174995883@gmail.com>
2025-09-18 08:43:35 +0530

    Defining Ports

--- server.js
+++ server.js
@@ -76,5 +76,5 @@ app.get("/file/:id", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
-
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
